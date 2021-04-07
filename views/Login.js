635a5bb1efaeec3e8703074061c243b051e0f6d6
edwin <183433@ids.upchiapas.edu.mx>
2021-04-07 00:03:50 -0500

    Consume API & starting visual design

--- views/Login.js
+++ views/Login.js
@@ -1,106 +1,107 @@
-import React, {useState} from 'react'
-import { View,Text, StyleSheet, Button,ScrollView, TextInput,Alert} from 'react-native'
+import React, { useState } from 'react'
+import { View, Text, StyleSheet, Button, ScrollView, TextInput, Alert } from 'react-native'
 
 
 const Login = (props) => {
 
-    const [state, setState] = useState({
-        user :'',
-        password: ''
-    });
+  const [state, setState] = useState({
+    user: '',
+    password: ''
+  });
 
-    const recuperarDatos = (name, value) => {
-        setState({...state,[name]: value});
-    };
+  const recuperarDatos = (name, value) => {
+    setState({ ...state, [name]: value });
+  };
 
-    const auth = async(str)=> {
-      if( str.user === '' && str.password === ''){
-        alert('No puede dejar los campos vacios')
-     }else if(str.user === ''){
-        alert('Ingrese nombre de Usuario o Email')
-      }else if(str.password === ''){ 
-        alert('Ingrese una Contraseña')
-      }else{
+  const auth = async (str) => {
+    if (str.user === '' && str.password === '') {
+      alert('No puede dejar los campos vacios')
+    } else if (str.user === '') {
+      alert('Ingrese nombre de Usuario o Email')
+    } else if (str.password === '') {
+      alert('Ingrese una Contraseña')
+    } else {
 
       const User = encodeURIComponent(str.user);
       const Password = encodeURIComponent(str.password);
       const requestBody = `user=${User}&password=${Password}`;
 
-      function obtenerToken(tkn){
-        console.log('token: ',tkn)
+      function obtenerToken(tkn) {
+        console.log('token: ', tkn)
       }
 
-     await fetch(`http://localhost:8080/user`, {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-            body: requestBody
-        })
-        .then(function(response){ 
-            response.json()
-          .then(function(responseServer) { 
-            obtenerToken(responseServer.token)
-             alert('Login Sucess!! :D')
-             props.navigation.navigate('Product')
+      await fetch(`http://localhost:8080/user`, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+        body: requestBody
+      })
+        .then(function (response) {
+          response.json()
+            .then(function (responseServer) {
+              obtenerToken(responseServer.token)
+              alert('Login Sucess!! :D')
+              //console.log(responseServer.token); asi esta bien escrito
+              props.navigation.navigate('Product', { token: responseServer.token })
             })
-            .catch(function(e){
+            .catch(function (e) {
               console.log(e);
-              alert('Lo sentimos, ah ocurrido un error ',e)
+              alert('Lo sentimos, ah ocurrido un error ', e)
             });
         })
-          .catch(function(e){
-              console.log(e);
-              alert(e)
-            });
-      } 
+        .catch(function (e) {
+          console.log(e);
+          alert(e)
+        });
     }
+  }
 
-    return (
-        <ScrollView style={styles.container}>
-            <View><Text style={styles.text}>AreSolutions Clinic</Text></View>
-            <View>
-                <TextInput style={styles.inputs} onChangeText={(value) => recuperarDatos('user',value)} placeholder="Email"/>
-            </View>
-            <View>
-                <TextInput style={styles.inputs}  secureTextEntry={true} onChangeText={(value) => recuperarDatos('password',value)} placeholder="Contraseña"/>
-            </View>
-            <View>
-            <Button color="#000000" onPress={() => auth(state)} title="Log in"/>
-            </View>
-        </ScrollView>
-    )
+  return (
+    <ScrollView style={styles.container}>
+      <View><Text style={styles.text}>AreSolutions Clinic</Text></View>
+      <View>
+        <TextInput style={styles.inputs} onChangeText={(value) => recuperarDatos('user', value)} placeholder="Email" />
+      </View>
+      <View>
+        <TextInput style={styles.inputs} secureTextEntry={true} onChangeText={(value) => recuperarDatos('password', value)} placeholder="Contraseña" />
+      </View>
+      <View>
+        <Button color="#000000" onPress={() => auth(state)} title="Log in" />
+      </View>
+    </ScrollView>
+  )
 }
 
 export default Login
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,     
-      padding:35
-    },
-    title:{
-      fontSize:20,
-      padding:10,
-      textAlign:'center',
-    },
-    inputs:{
-      backgroundColor:"#f1f1f1",
-      borderWidth:1,
-      textAlign:'center',
-      /* width:300, */
-      margin:10,
-      alignItems:'stretch'
-    },
-    btn:{
-      backgroundColor: "#20B685",
-      borderRadius:50,
-      height:35,
-      alignItems:'center'
-    },
-    text:{
-      fontSize:20,
-      padding:5,
-      alignSelf:'center',
-      color:'black',
-    },
+  container: {
+    flex: 1,
+    padding: 35
+  },
+  title: {
+    fontSize: 20,
+    padding: 10,
+    textAlign: 'center',
+  },
+  inputs: {
+    backgroundColor: "#f1f1f1",
+    borderWidth: 1,
+    textAlign: 'center',
+    /* width:300, */
+    margin: 10,
+    alignItems: 'stretch'
+  },
+  btn: {
+    backgroundColor: "#20B685",
+    borderRadius: 50,
+    height: 35,
+    alignItems: 'center'
+  },
+  text: {
+    fontSize: 20,
+    padding: 5,
+    alignSelf: 'center',
+    color: 'black',
+  },
 
-  });
+});
--- views/Product.js
+++ views/Product.js
@@ -1,13 +1,124 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { useEffect } from 'react';
+import { useState } from 'react';
+import { Image } from 'react-native';
+import { Button, ScrollView, StyleSheet, Text, View } from 'react-native';
+import { Avatar, Card, Icon, ListItem } from 'react-native-elements';
 
-const Product = () => {
-    return(
-        <View style={{flex:1,alignSelf:'center', padding:20}}>
-            <Text>Esta es la vista despues de LOGEARSE xd</Text>
-        </View>
+const Product = (props) => {
+
+    //console.log("token en product: ", props.route.params.token);
+    const token = props.route.params.token
+
+    const [products, setProducts] = useState([])
+
+    useEffect(() => {
+        const products = []
+        fetch('http://localhost:8080/API/tiendaJSON', {
+            method: 'GET',
+            headers: {
+                Authorization: 'Bearer ' + token,
+                'Content-Type': 'application/json;charset=UTF-8',
+                'accept': 'application/json;charset=UTF-8'
+            }
+        })
+            .then((response) => response.json())
+            .then((repsonseJson) => {
+                /* console.log("response: ", repsonseJson); */
+                for (let i = 0; i < repsonseJson.tiendasList.length; i++) {
+                    const { id, name, description, price, existence, photo } = repsonseJson.tiendasList[i]
+                    products.push({
+                        id,
+                        name,
+                        description,
+                        price,
+                        existence,
+                        photo
+                    })
+                }
+                console.log(products);
+                setProducts(products)
+                console.log("size tienda: ", repsonseJson.tiendasList.length);
+            })
+            .catch((error) => {
+                console.log(error);
+            })
+    }, [])
+
+    return (
+        <ScrollView>
+            <Text style={styles.title} > Tienda Online</Text >
+            {
+                products.map(product => {
+                    const urlPhoto = 'http://localhost:8080' + product.photo
+                    return (
+                        /*                         <ListItem key={product.id} bot>
+                                                    <ListItem.Chevron />
+                                                    <ListItem.Content>
+                                                        <ListItem.Title>{product.name}</ListItem.Title>
+                                                        <ListItem.Subtitle>{product.description}</ListItem.Subtitle>
+                                                    </ListItem.Content>
+                                                </ListItem> 
+                                                <Avatar source={{ uri: prueba }} rounded />*/
+                        <Card key={product.id}>
+                            <Card.Title style={styles.titleCard}>{product.name}</Card.Title>
+                            <Card.Divider />
+                            <Card.Image source={{ uri: urlPhoto }} style={styles.imageContent}></Card.Image>
+                            <Card.Divider />
+                            <Text style={styles.textContentCard}>{product.description}</Text>
+                            <Text style={styles.textContentCard}>Precio: {product.price}</Text>
+                            <Text style={styles.textContentCard}>Existencia: {product.existence}</Text>
+                            <Button
+                                icon={<Icon name='code' color='#ffffff' size='40' type='clear' />}
+                                buttonStyle={{ borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 0, }}
+                                title='BUY'
+                            />
+                        </Card>
+                    )
+                })
+            }
+        </ScrollView>
     )
 }
 
-export default Product;
+const styles = StyleSheet.create({
+    container: {
+        flex: 0,
+        alignSelf: 'center',
+        padding: 10,
+    },
+    title: {
+        marginTop: 16,
+        paddingVertical: 8,
+        borderWidth: 4,
+        borderColor: '#20232a',
+        borderRadius: 6,
+        backgroundColor: '#61dafb',
+        color: '#20232a',
+        textAlign: 'center',
+        fontSize: 30,
+        fontWeight: 'bold',
+    },
+    titleCard: {
+        fontSize: 30,
+        fontWeight: 'bold'
+    },
+    textContentCard: {
+        fontStyle: 'italic',
+        textAlign: 'center',
+        fontSize: 20,
+        paddingBottom: 5
+    },
+    imageContent: {
+        width: 300,
+        height: 200,
+        /*         position: 'relative', */
+        resizeMode: 'contain'
+    },
+    buttonText: {
+        fontSize: 25,
+        fontWeight: 'bold'
+    }
+})
 
+export default Product
